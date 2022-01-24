87cb766ac841ac56e4aaf0840293879a338c5f78
Kartikkala <kartikkala10december@gmail.com>
2022-01-24 19:55:39 +0530

    Made matrix multiplication code for 3X3 and 2X2 matrix

--- matrixmul.c
+++ matrixmul.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int a[2][2]={{1,2},{3,4}},b[2][2]={{1,2},{3,4}},c[2][2],i=0,j=0;
-    for (;i<2;)
+    int a[2][2]={{1,2},{3,4}},b[2][2]={{1,2},{3,4}},c[2][2];
+    for (int i=0;i<2;++i)
     {
-        for(;j<2;)
+        for(int j=0;j<2;++j)
         {
-            c[i][j]=a[i][j]*b[i][j]+a[i][j]*b[i][j];
+            c[i][j]=0;
+            for(int k=0;k<2;++k)
+            {
+                c[i][j]+=a[i][k]*b[k][j];
+            }
         }
     }
+    for (int i=0;i<2;++i)
+    {printf("\n");
+        for(int j=0;j<2;++j)
+        {
+            printf("\t%d",c[i][j]);
+        }};
+
     return 0;
 }
