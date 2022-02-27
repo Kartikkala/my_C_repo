0b30db9c589c0c33d66b871cd8850193e8ef0418
Kartikkala <kartikkala10december@gmail.com>
2022-02-27 15:08:12 +0530

    Updated the pyramidal triangle program to print a triangle full of numbers instead of asterisks '*'

--- pattern_of_tr.c
+++ pattern_of_tr.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 int main(void)
 {
-    
-    for(int n=2,m=5,i=1;i<=9;i=i+2)
-    {printf("\n");
+    for(int counter,n=1,m=5,i=1;i<=9;i=i+2,n++)
+    {
+        printf("\n");
+        counter=n;
         for(int j=1;j<=9;j++)
         {
             if(j==m)
             {
             for(int k=1;k<=i;k++)
             {
-                if(k%2!=0) 
+                                     //midpoint = n counter is that number from where we have to start counting of a row until the end of that number(counter)
+                if(k==n) 
                 {
                 printf("%d",i); 
+                counter--;
                 }
                 else
                 {
-                 printf("%d",n);   
+                 printf("%d",counter);
+                 if(k<n)
+                 {
+                     counter++;
+                 }   
+                 else{counter--;}
                 }
-            }m=m-1;
+            }m=m-1;break;
             }
             else
             {
